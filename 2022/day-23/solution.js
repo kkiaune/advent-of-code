4db6f74532d96d1c9b75ece1b7797b4fc281861c
Karolis <kiaune.karolis@gmail.com>
2025-12-16 10:34:14 +0200

    cleanup

--- 2022/day-23/solution.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-let list = fs.readFileSync("example.txt", "utf-8");
-list = list.split(/\r?\n/);
-
-console.log("first part result: TBD");
-console.log("second part result: TBD");
